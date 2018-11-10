b74abce8a5c9e9770ff254b0fbf5cdf6cb959227
Scott Clarke <sdclarke123@gmail.com>
2018-11-10 22:08:44 +0000

    Added javascript to HouseInfo.php

--- HouseInfo.php
+++ HouseInfo.php
@@ -40,7 +40,7 @@ textarea {
 }
 
 button {
-  
+
 }
 div {
     border-radius: 5px;
@@ -57,26 +57,97 @@ form {
 
 <body>
   <form action ="PutDataInTable.php" method="POST">
-  First name: 
-  <input type="text" name="firstname"><br>
-  Last name: 
-  <input type="text" name="lastname"><br>
-  House address: 
-  <input type="text" name="address"><br>
-  Total capacity: 
-  <input type="text" name="capacity"><br>
-  Spaces: 
-  <input type="text" name="spaces"><br>
-  Email: 
-  <input type="text" name="email"><br>
-  Contact number (not necessary): 
-  <input type="text" name="number"><br>
-  Description:<br>
-  <textarea maxlength="280" rows="5" name="description"></textarea><br>
-  House password:<br>
-  <input type="text" name="pwd"><br>
-  <button type="submit" name="submit">Submit</button>
-</form>
+    First name:
+    <input type="text" name="firstname"><br>
+    Last name:
+    <input type="text" name="lastname"><br>
+    House address:
+    <input type="text" name="address">
+    <button type="button" name="show">Show on map</button><br>
+    <input type="hidden" name="latitude">
+    <input type="hidden" name="longitude">
+    Total capacity:
+    <input type="text" name="capacity"><br>
+    Spaces:
+    <input type="text" name="spaces"><br>
+    Email:
+    <input type="text" name="email"><br>
+    Description:<br>
+    <textarea maxlength="280" rows="5" name="description"></textarea><br>
+    House password:<br>
+    <input type="text" name="pwd"><br>
+    <button type="submit" name="submit">Submit</button>
+  </form>
+  <script>
+    var map, infoWindow, marker, geocoder;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 46.3256, lng: 5.36194},
+        zoom: 4
+      });
+      infoWindow = new google.maps.InfoWindow;
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+
+          map.setCenter(pos);
+          map.setZoom(8);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+
+      geocoder = new google.maps.Geocoder();
+
+      document.getElementById('show').addEventListener('click', function() {
+      geocodeAddress(geocoder, map);
+      });
+
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+    }
+
+    function geocodeAddress(geocoder, resultsMap) {
+      if (marker != null){
+        marker.setMap(null);
+      }
+      var address = document.getElementById('address').value;
+      geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+          resultsMap.setCenter(results[0].geometry.location);
+          marker = new google.maps.Marker({
+            map: resultsMap,
+            position: results[0].geometry.location
+          });
+          map.setZoom(16);
+          document.getElementById('latitude').value = results[0].geometry.location.lat;
+          document.getElementById('longitude').value = results[0].geometry.location.lng;
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
+    }
+    /*function validate(){
+      var address = document.getElementById('address').value;
+    }*/
+  </script>
+  <script async defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDgYKliWrj5R0-7dXRZIagge7vPkTSzHFY&callback=initMap">
+  </script>
 </body>
 </html>
-
