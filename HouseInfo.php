69ccf7eee2cadec1dbe788e0bfc63a7e72ad774f
EvgenySerebrenny <EvgenySerebrenny@users.noreply.github.com>
2018-11-10 19:52:48 +0000

    Update HouseInfo.html

---
+++ HouseInfo.php
@@ -0,0 +1,100 @@
+<<<<<<< HEAD:HouseInfo.html
+<!DOCTYPE html>
+<html>
+
+<style>
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+body {
+    background-color: #c8cbd1;
+}
+input[type=submit] {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+textarea {
+  	resize: none;
+  	width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+button {
+	
+}
+div {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+body {
+    text-align: center;
+}
+form {
+    display: inline-block;
+}
+</style>
+
+<body>
+  <form>
+  First name: 
+=======
+<form>
+  First name:
+>>>>>>> 59952b19c846720407a503e585461371e4b8ad7d:HouseInfo.php
+  <input type="text" name="firstname"><br>
+  Last name:
+  <input type="text" name="lastname"><br>
+  House address:
+  <input type="text" name="firstname"><br>
+  Total capacity:
+  <input type="text" name="lastname"><br>
+  Spaces:
+  <input type="text" name="lastname"><br>
+  Email:
+  <input type="text" name="firstname"><br>
+  Contact number (not necessary):
+  <input type="text" name="lastname"><br>
+  Spaces:
+  <input type="text" name="lastname"><br>
+<<<<<<< HEAD:HouseInfo.html
+  Description:<br>
+  <textarea maxlength="280" rows="5"></textarea><br>
+  <button type="submit" name="submit">Submit</button>
+</form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+=======
+</form>
+>>>>>>> 59952b19c846720407a503e585461371e4b8ad7d:HouseInfo.php
---
+++ getData.php
@@ -0,0 +1,43 @@
+<?php
+  $doc = domxml_new_doc("1.0");
+  $node = $doc->create_element("houses");
+  $parnode = $doc->append_child($node);
+
+  $connection=mysql_connect('wdit34254jcrnon57cewcbigvi@speckle-umbrella-22.iam.gserviceaccount.com', $username, $password);
+  if(!$connection)
+  {
+    die('Not connection: ' . mysql_error());
+  }
+
+  $db_selected = mysql_select_db($database, $connection);
+  if (!$db_selected)
+  {
+    die ('Can\'t use db : ' . mysql_error());
+  }
+
+  $query = "SELECT * FROM house";
+  $result = mysql_query($query);
+  if(!$result)
+  {
+    die('Invalid query: ' . mysql_error());
+  }
+
+  header("Content-type: text/xml");
+
+  while($row = @mysql_fetch_assoc("houses"))
+  {
+    $node = $doc->create_element("house");
+    $newnode = $parnode->append_child($node);
+
+    $newnode->set_attribute("id", $row['id']);
+    $newnode->set_attribute("address", $row['address']);
+    $newnode->set_attribute("latitude", $row['latitude']);
+    $newnode->set_attribute("longitude", $row['longitude']);
+    $newnode->set_attribute("totalcapacity", $row['totalcapacity']);
+    $newnode->set_attribute("spaces", $row['spaces']);
+    $newnode->set_attribute("description", $row['description']);
+    $newnode->set_attribute("personID", $row['personID']);
+  }
+  $xmlfile = $doc->dump_mem();
+  echo $xmlfile;
+ ?>
